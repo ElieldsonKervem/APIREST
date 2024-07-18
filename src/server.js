95f41a270f878ad8effa9a77b1842a2d69e6eba7
ElieldsonKervem <kervem.elieldson@academico.ifrn.edu.br>
2024-07-18 20:56:30 -0300

    Crud completed end of project

--- src/server.js
+++ src/server.js
@@ -4,21 +4,53 @@ const server = express();
 const port = 3333;
 server.use(express.json())
 
-server.get('/curso',(req,res)=>{
-    const nome = req.query.nome
-    const id = req.params.id
-    return res.send('Rota funcionando'+ ' ' + nome + ' ' + `O numero do curso é ${id}`)
+const cursos = ['javascript','nodejs','reactJS'];
 
-    //query paramas são sempre enviado como objeto ? antes e chave=valor & para adicionar mais um query
+//MIDDLEWARE
+function checkMiddleware(req,res,next){
+    if(!req.body.CourseName){
+      return  res.status(400).json({erro:"Nome do curso é obrigatorio"})
+    }
+    return next();
+}
+
+server.get('/cursos',(req,resp)=>{
+    return resp.json(cursos)
+}) 
+server.get('/cursos/:index',checkMiddleware,(req,res)=>{
+    const {index} = req.params
+    return res.json(cursos[index])
+   //query paramas são sempre enviado como objeto ? antes e chave=valor & para adicionar mais um query
 })
-//posso querer pegar via router params
 
-//vamos ver como funciona os queryParams
-//vamos ver como funciona o router params
-//vamos ver como funciona o body params
+//CREATE
+server.post('/cursos',(req,res)=>{
+      const {CourseName} = req.body;
+      cursos.push(CourseName) 
+      return res.json(cursos)  
+})
+
+//UPDATE
+
+server.put('/cursos/:index',(req,res)=>{
+     const {index} =  req.params
+     const {name} = req.body;
+
+     cursos[index] = name;
+
+     return res.json(cursos) 
+})
+
+server.delete('/cursos/:index',(req,res)=>{
+    const {index} = req.params;
+    cursos.splice(index,1);
+    return res.json(cursos)
+})
+
 
 server.listen(port,()=>{
     console.log('servidor rodando na porta' + port);
-})
+});
 
 
+//vamos ver como criar um crud
